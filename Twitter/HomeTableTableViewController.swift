c70e4546bb747003268be4146b58261078a9ed5d
Tahmid Zaman <tahmid198@yahoo.com>
2021-02-26 23:53:34 -0500

    created tableViewCell for name image and label

--- Twitter/HomeTableTableViewController.swift
+++ Twitter/HomeTableTableViewController.swift
@@ -13,25 +13,38 @@ class HomeTableTableViewController: UITableViewController {
     @IBAction func onLogout(_ sender: Any) {
         TwitterAPICaller.client?.logout()
         self.dismiss(animated: true, completion: nil)
+        UserDefaults.standard.set(false, forKey: "userLoggedIn") //will allow user to log out
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! tweetCell
+        
+        cell.userNameLabel.text = "Some name"
+        cell.tweetContent.text = "some content"
+        
+        return cell
+        
+       
         
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+    
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 5
     }
 
 }
--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -9,23 +9,34 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    //Will allow user to stay loggedin even after closing application
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true{
+            self.performSegue(withIdentifier: "LoginToHome", sender: self)
+        }
+        
+    }
+    
     @IBAction func onLoginButton(_ sender: Any) {
         
         //Action is linked to button. Once button is clicked we call out API caller with URL. If it works we perform seque, if not we print "Could not login"
         let myURL = "https://api.twitter.com/oauth/request_token"
-        TwitterAPICaller.client?.login(url: myURL, success:{ self.performSegue(withIdentifier: "LoginToHome", sender: self)
+        TwitterAPICaller.client?.login(url: myURL, success:{
+            
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
+            self.performSegue(withIdentifier: "LoginToHome", sender: self)
         }, failure: {(Error) in print("Could not login!")
             
         })
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
     /*
     // MARK: - Navigation
 
